accf924cc07dea5896d8d7b70549e035f3e9ed0a
Orlando Fernández <orlando_fg@hotmail.com>
2021-06-12 05:27:58 +0200

    Revert "Ampliar Test"
    This reverts commit 3522fdf4978b15771ffb6ad0a5f6b3046411f685.

--- BiosystemsTest/Clases/PuntuacionClass.cs
+++ BiosystemsTest/Clases/PuntuacionClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BiosystemsTest.Clases
 {
-    public class PuntuacionClass
+    class PuntuacionClass
     {
         public PuntuacionClass()
         {
---
+++ BiosystemsTestTests/Clases/SetClassTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiosystemsTest.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiosystemsTest.Clases.Tests
+{
+    [TestClass()]
+    public class SetClassTests
+    {
+
+        [TestMethod()]
+        public void GanaTest()
+        {
+            int countEsperado = 1;
+            int pointEsperado = 5;
+            int juegosEsperado = 2;
+
+            SetClass set = new();
+            for (int i = 0; i < 5; i++)
+            {
+                if (set.Gana())
+                {
+                    set.CurrentJuego.EsGanador = true;
+                    set.AddJuego();
+                }
+            }
+            Assert.AreEqual(countEsperado, set.CountJuegosGanados);
+            Assert.AreEqual(pointEsperado, set.CountPuntos);
+            Assert.AreEqual(juegosEsperado, set.Juegos.Count);
+        }
+
+
+    }
+}
